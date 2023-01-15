432b80df24f1876346e23accd025323276559e10
DmitryMix260 <dmitry-buyanov1995@yandex.ru>
2023-01-15 13:34:53 +0300

    Render board from cicly

--- src/index.js
+++ src/index.js
@@ -18,29 +18,20 @@ function Square(props){
     renderSquare(i) {
       return (<Square 
             value={this.props.squares[i]}
-            onClick={()=> this.props.onClick(i)}
+            onClick={()=> {this.props.onClick(i)}}
         />
         );
     }
   
     render() {
+      const rows = 3;
+      const colls = 3;
       return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+          {[...Array(rows).keys()].map(row => (<div className="board-row" key="{row}">
+            {[...Array(colls).keys()].map(coll =>this.renderSquare(row*colls+coll))}
+          </div>))
+          }
         </div>
       );
     }
